0571349a5d1e654d9b0b054679e7ef642488395c
ingaproblemil <l.emil.j.johansson@gmail.com>
2022-09-19 19:43:56 +0200

    add playRound function

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function getComputerChoice(max) {
   }
   
   console.log(getComputerChoice(3));
+
+function playRound(playerSelection, computerSelection) {
+    
+    if ((playerSelection.toUpperCase() === "ROCK") && (computerSelection === "Scissors")) {
+        
+        return "WIN";
+        } else {
+
+        return "You Lose! Paper beats Rock!";
+} }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
