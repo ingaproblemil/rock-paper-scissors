922d508c55c92abfbf4fd746fc541581dfee6b79
ingaproblemil <l.emil.j.johansson@gmail.com>
2022-09-20 13:51:57 +0200

    add playRound function

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-let rPSArray = ["Rock", "Paper", "Scissors"] 
+const rPSArray = ["Rock", "Paper", "Scissors"] 
 
 function getComputerChoice(max) {
-    return rPSArray[Math.floor(Math.random() * max)];
+    return rPSArray[Math.floor(Math.random() * 3)];
   }
   
-  console.log(getComputerChoice(3));
+  // console.log(getComputerChoice());
+  
+
+
 
 function playRound(playerSelection, computerSelection) {
     
-    if ((playerSelection.toUpperCase() === "ROCK") && (computerSelection === "Scissors")) {
-        
-        return "WIN";
-        } else {
+    if 
+    (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Scissors') {
+        return "You win! Rock beats scissors!";
 
-        return "You Lose! Paper beats Rock!";
-} }
+    } else if
+      (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Paper'){
+        return "You win! Scissors beats paper!";
+      
+    } else if 
+      (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Rock') {
+          return "You win! Paper beats rock!";
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+    } else if
+      (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Paper') {
+        return "You lose! Paper beats rock!"
+      
+    } else if
+      (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Scissors') {
+        return "You lose! Scissors beats paper!"
+
+    } else if 
+      (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Rock') {
+        return "You lose! Rocks beats scissors!"
+    
+    } else {
+        return "It's a tie!"
+      }
+}
+
+function game(playRound) {}
+
+const playerSelection = prompt();
+const computerSelection = getComputerChoice(3);
 
 console.log(playRound(playerSelection, computerSelection));
+console.log("Computer plays: "+computerSelection)
+console.log("Human plays: "+playerSelection)
