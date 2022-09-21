989cef5280dc3bddae008246865599aba3214bcf
ingaproblemil <l.emil.j.johansson@gmail.com>
2022-09-21 13:00:26 +0200

    add function counter

--- index.js
+++ index.js
@@ -4,52 +4,79 @@ function getComputerChoice(max) {
     return rPSArray[Math.floor(Math.random() * 3)];
   }
   
+  let playerScore = 0;
+  let computerScore = 0;
+  let roundsPlayed = 0;
+
 function playRound(playerSelection, computerSelection) {
+ 
   
     if 
     (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Scissors') {
-        return "playerPoint";
+      playerScore++;
+      roundsPlayed++;  
+      return counter();
 
     } else if
       (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Paper') {
-        return "playerPoint";
+        playerScore++;
+        roundsPlayed++;
+        return counter();
       
     } else if 
       (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Rock') {
-          return "playerPoint";
+        playerScore++;
+        roundsPlayed++;
+          return counter();
 
     } else if
       (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Paper') {
-        return "computerPoint";
+        computerScore++;
+        roundsPlayed++;
+        return counter();
       
     } else if
       (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Scissors') {
-        return "computerPoint";
+        computerScore++;
+        roundsPlayed++;
+        return counter();
 
     } else if 
       (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Rock') {
-        return "computerPoint";
+        computerScore++;
+        roundsPlayed++;
+        return counter();
     
     } else if 
       (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        return "It's a tie!";
+        roundsPlayed++;
+        return counter();
 
     } else {
         return "You must enter Rock, Paper or Scissors din dumma lilla Ulf Kristersson"
         }
+       
       }
 
-function game(playRound) {
+function counter() {
 
-}
+      console.log("Player: "+playerScore)
+      console.log("Computer: "+computerScore) 
+      console.log("Rounds played: "+roundsPlayed)
+}      
 
-const playerSelection = prompt();
+
+const playerSelection = prompt("Make your pick: Rock, Paper or Scissors!");
 const computerSelection = getComputerChoice(3);
 
-const playerPoint = playRound(playerSelection, computerSelection)=="playerPoint"
-const computerPoint  =playRound(playerSelection, computerSelection)=="computerPoint"
+// let playerPoint = playRound(playerSelection, computerSelection)=="playerPoint";
+// let computerPoint = playRound(playerSelection, computerSelection)=="computerPoint";
+
+let round = playRound(playerSelection, computerSelection);
 
 
-console.log(playRound(playerSelection, computerSelection));
+
+
+// console.log(playRound(playerSelection, computerSelection));
 console.log("Computer plays: "+computerSelection);
 console.log("Human plays: "+playerSelection.substr(0, 1).toUpperCase()+playerSelection.substr(1).toLowerCase());
